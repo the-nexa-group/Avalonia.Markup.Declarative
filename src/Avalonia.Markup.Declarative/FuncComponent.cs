551398cd10222952f4353a17351f76f762adc3b9
dynamiquel <dynamiquel@outlook.com>
2026-01-30 00:46:51 +0000

    func data templates

--- src/Avalonia.Markup.Declarative/FuncComponent.cs
+++ src/Avalonia.Markup.Declarative/FuncComponent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Avalonia.Controls;
+using Avalonia.Controls.Templates;
 
 namespace Avalonia.Markup.Declarative;
 
@@ -7,3 +8,10 @@ public class FuncComponent<TViewModel>(TViewModel model, Func<TViewModel, Contro
 {
     protected override Control? Build() => build.Invoke(model);
 }
+
+/// <summary>
+/// Effectively combines FuncDataTemplate and FuncComponent into a single command; reducing boilerplate.
+/// Useful for when you want to use special bindings for a control generated within an Item Template.
+/// </summary>
+public class FuncCompDataTemplate<TViewModel>(Func<TViewModel, Control> build)
+    : FuncDataTemplate<TViewModel>((model, _) => new FuncComponent<TViewModel>((TViewModel?)model!, build));
--- src/Avalonia.Markup.Declarative/FuncView.cs
+++ src/Avalonia.Markup.Declarative/FuncView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Avalonia.Controls;
+using Avalonia.Controls.Templates;
 
 namespace Avalonia.Markup.Declarative;
 
@@ -7,3 +8,11 @@ public class FuncView<TViewModel>(TViewModel model, Func<TViewModel, Control?> b
 {
     protected override Control? Build(TViewModel? vm) => build.Invoke(vm!);
 }
+
+/// <summary>
+/// Effectively combines FuncDataTemplate and FuncView into a single command; reducing boilerplate.
+/// Useful for when you want to use special bindings for a control generated within an Item Template.
+/// </summary>
+public class FuncViewDataTemplate<TViewModel>(Func<TViewModel, Control> build)
+    : FuncDataTemplate<TViewModel>((model, _) => new FuncView<TViewModel>((TViewModel?)model!, build));
+    
